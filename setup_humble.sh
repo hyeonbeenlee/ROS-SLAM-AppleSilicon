050bd9851b25b9b51dd7035c6a33927737312461
HB's Macbook Pro <hb_pen@naver.com>
2023-10-30 20:02:05 +0900

    update

--- setup_humble.sh
+++ setup_humble.sh
@@ -89,14 +89,9 @@ sudo apt install ros-humble-cartographer -y
 sudo apt install ros-humble-cartographer-ros -y
 sudo apt install ros-humble-navigation2 -y
 sudo apt install ros-humble-nav2-bringup -y
-mkdir -p ~/turtlebot3_ws/src
-cd ~/turtlebot3_ws/src/
-git clone -b humble-devel https://github.com/ROBOTIS-GIT/DynamixelSDK.git
-git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
-git clone -b humble-devel https://github.com/ROBOTIS-GIT/turtlebot3.git
-cd ~/turtlebot3_ws
-colcon build --symlink-install
-echo 'source ~/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
+sudo apt install ros-humble-dynamixel-sdk -y
+sudo apt install ros-humble-turtlebot3-msgs -y
+sudo apt install ros-humble-turtlebot3 -y
 echo 'export ROS_DOMAIN_ID=30 #TURTLEBOT3' >> ~/.bashrc
 source ~/.bashrc
 cd ~/turtlebot3_ws/src/
