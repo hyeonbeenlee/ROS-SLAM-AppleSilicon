dcf8508de9e8901cc3df56617e7fefffbe5bb5e5
WSL-Hyeonbeen <lhbsharp@khu.ac.kr>
2023-10-30 19:45:40 +0900

    pass -y

--- setup_humble.sh
+++ setup_humble.sh
@@ -69,25 +69,25 @@ sudo DEBIAN_FRONTEND=noninteractive apt install code -y
 
 
 # ROS-Humble installation
-sudo apt update && sudo apt install locales
+sudo apt update && sudo apt install locales -y
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository universe
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update && sudo apt upgrade -y
-sudo apt install ros-humble-desktop
+sudo apt install ros-humble-desktop -y
 echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 # ROS Turtlebot3
-sudo apt install ros-humble-gazebo-*
-sudo apt install ros-humble-cartographer
-sudo apt install ros-humble-cartographer-ros
-sudo apt install ros-humble-navigation2
-sudo apt install ros-humble-nav2-bringup
+sudo apt install ros-humble-gazebo-* -y
+sudo apt install ros-humble-cartographer -y
+sudo apt install ros-humble-cartographer-ros -y
+sudo apt install ros-humble-navigation2 -y
+sudo apt install ros-humble-nav2-bringup -y
 mkdir -p ~/turtlebot3_ws/src
 cd ~/turtlebot3_ws/src/
 git clone -b humble-devel https://github.com/ROBOTIS-GIT/DynamixelSDK.git
